8ae5e3cd63bad8f07e1c71dde9c172981f0a267c
Ahmed-ayed <ahmedayed576@gmail.com>
2024-01-10 00:37:47 +0200

    Fixed Some Bugs

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,36 +1,19 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]
-then
+if [ "$#" -ne 2 ]; then
     echo "Error: Please provide two arguments - full path to a file and a text string to write"
     exit 1
 fi
 
-writefile=$1
-writestr=$2
-
-if [ -z "$writefile" ]
-then
-    echo "Error: File path not specified"
-    exit 1
-fi
-
-if [ -z "$writestr" ]
-then
-    echo "Error: Text string not specified"
-    exit 1
-fi
+writefile="$1"
+writestr="$2"
 
 mkdir -p "$(dirname "$writefile")"
 
 echo "$writestr" > "$writefile"
 
-if [ $? -ne 0 ]
-then
+if [ "$?" -ne 0 ]; then
     echo "Error: Failed to create the file $writefile"
     exit 1
 fi
 
-echo "File $writefile created with content"
-echo "$writestr"
-exit 0
